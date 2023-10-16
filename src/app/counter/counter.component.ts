9f6fcb3dec40b5103aebe60329e16d28f68da22a
BhanuTejaPokala <145679466+BhanuTejaPokala@users.noreply.github.com>
2023-10-16 20:06:53 +0530

    animation

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-counter-component',
-  templateUrl: './counter.component.html'
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  ngOnInit() {
+         // Get a reference to the animation container element by its ID
+         const animationContainer = this.el.nativeElement.querySelector('#animation-container');
 
+     
+         // Function to hide the animation container
+         const hideAnimationContainer = () => {
+           this.renderer.setStyle(animationContainer, 'display', 'none');
+         };
+     
+          // Set a timeout to hide the animation container after 10 seconds (10000 milliseconds)
+          setTimeout(hideAnimationContainer, 10000);
+}
 }
--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, Renderer2 } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,10 +6,32 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html',
   styleUrls: ['./fetch-data.component.css']
 })
-export class FetchDataComponent { 
+export class FetchDataComponent implements OnInit {
   isClosed = false;
+  iframeActive = true; // Track whether the iframe is active
+  
+  ngOnInit() {
+    this.iframeActive = false; // Initialize it to false during component initialization
+  
+    // Call a function to refresh the iframe every 20 minutes
+    setInterval(this.refreshIframe, 1200000);
+
+
+  }
+
 
   toggleChatbot() {
-    this.isClosed = !this.isClosed;
+    if (!this.iframeActive) {
+      this.isClosed = !this.isClosed;
+      }
+  }
+
+ 
+  // Function to refresh the iframe
+  refreshIframe() {
+    const iframe = document.getElementById('chatbot-iframe') as HTMLIFrameElement;
+    if (iframe) {
+      iframe.src = iframe.src; // Reload the iframe by setting its src attribute to itself
+    }
   }
 }
